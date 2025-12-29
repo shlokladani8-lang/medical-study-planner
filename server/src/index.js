0b85809c9d9975edaf07d07955d718940183b839
shlokladani8-lang <shlokladani8@gmail.com>
2025-12-29 19:09:41 +0530

    Add basic backend server

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
