ccad3e2d84a29325cacc364f3629b00547dbe7d3
shlokladani8-lang <shlokladani8@gmail.com>
2025-12-29 19:17:19 +0530

    Initialize Prisma client

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend running");
+app.get("/", async (req, res) => {
+  res.json({ status: "Backend with Prisma ready" });
 });
 
 app.listen(PORT, () => {
